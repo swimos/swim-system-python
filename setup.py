744042d8e1abbe92b0b0d53d5344f0429f1fe3b1
DobromirM <mr.d.marinov@gmail.com>
2019-10-23 17:30:44 +0100

    * Updated version number * Added deploy phase for Travis CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='swimai',
-    version='0.0.1.dev2',
+    version='0.0.1-dev.3',
     author='Dobromir Marinov',
     author_email='dobromir@swim.it',
     description='Standalone Python framework for building massively real-time streaming WARP clients.',
