0e6f1d567d975c42c22bcc5f62ef659b575f1493
DobromirM <mr.d.marinov@gmail.com>
2019-10-23 16:01:37 +0100

    * Fixed project name mismatch

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='swim',
+    name='swimai',
     version='0.0.1.dev2',
     author='Dobromir Marinov',
     author_email='dobromir@swim.it',
