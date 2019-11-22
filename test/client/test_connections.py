8528383d57304d681b23b6e4a928e8867ce066a1
DobromirM <mr.d.marinov@gmail.com>
2019-11-22 13:08:30 +0000

    ---Closes #31---
    * Added copyright to all Python files.

--- test/client/test_connections.py
+++ test/client/test_connections.py
@@ -1,3 +1,17 @@
+#  Copyright 2015-2019 SWIM.AI inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import unittest
 # from unittest.mock import patch
 from aiounittest import async_test
@@ -7,11 +21,7 @@ from swimai.client import WSConnection, ConnectionStatus
 from test.utils import MockWebsocket
 
 
-# AsyncMock
-
-
 class TestWsConnections(unittest.TestCase):
-
     def setUp(self):
         MockWebsocket.clear()
 
