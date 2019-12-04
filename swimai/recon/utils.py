8861b89b2452e8d3af7d96bb37a7ffc849de73e1
DobromirM <mr.d.marinov@gmail.com>
2019-12-04 15:08:26 +0000

    ---Testing pull request process---

--- swimai/recon/utils.py
+++ swimai/recon/utils.py
@@ -242,5 +242,5 @@ class InputMessage(Message):
 
         :return:                - The new head character of the InputMessage.
         """
-        self.index = self.index + 1
+        self.index = self.index + 2
         return self.head
